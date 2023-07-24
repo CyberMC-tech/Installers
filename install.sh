e3c6312ae1f06f977de9bee01899915f13f89228
elliot <elliot@fsociety>
2023-07-24 13:51:52 -0400

    updated fnm to work in python, changed program in install.sh from vscode to code, etc

--- install.sh
+++ install.sh
@@ -6,12 +6,13 @@ sleep 3
 clear 
 printf "Updating system...\n" 
 sleep 3 
-sudo apt -qq update && sudo apt upgrade -y > /dev/null 2>&1 
+sudo apt -q update && sudo apt upgrade -y  
 sleep 3
 clear 
 printf "installing packages needed for main install\n" 
 sleep 3 
-sudo apt -qq install nala python3 python3-pip > /dev/null 2>&1 
+sudo apt -q install nala python3 python3-pip  
+pip install tqdm
 clear 
 printf "Required packages successfully installed\n" 
 sleep 3 
